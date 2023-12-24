3969a46b267382074512d7d260d40f9613814eca
galangrs <galang@samdues.com>
2023-12-24 18:07:02 +0700

    update postgres

--- postgres/postgres.go
+++ postgres/postgres.go
@@ -7,7 +7,32 @@ import (
 	"gorm.io/gorm"
 )
 
-// DBConnect establishes a connection to a PostgreSQL database
+/*
+DBConnect establishes a connection to a PostgreSQL database
+Parameters:
+  - host: The host of the database.
+  - port: The port of the database.
+  - username: The username for authentication.
+  - password: The password for authentication.
+  - dbname: The name of the database.
+  - models: Optional variadic parameter for specifying GORM models to auto-migrate.
+
+Returns:
+  - db: A pointer to the gorm.DB object representing the database connection.
+  - err: An error, if any.
+
+“
+
+	func main() {
+		db, err := DBConnect("127.0.0.0","5432","postgres","postgres","postgres"")
+		if err != nil {
+			log.Fatal("Failed to connect to database:", err)
+		}
+		defer db.Close()
+	}
+
+“
+*/
 func DBConnect(host string, port string, username string, password string, dbname string, models ...interface{}) (*gorm.DB, error) {
 	// create the DSN string for connecting to the database
 	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", host, port, username, dbname, password)
